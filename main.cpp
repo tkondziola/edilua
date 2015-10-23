5451608d798cf01a4420e3c8eb5deba03850d4fa
Tomasz Kondziola <tkondziola@gmail.com>
2015-10-23 15:18:08 +0200

    Splash screen commented out (for now)

--- main.cpp
+++ main.cpp
@@ -13,18 +13,16 @@ int main(int argc, char *argv[])
 
     // Splash screen
     QPixmap pixmap(":/edilua/resources/splashscreen/logo.svg");
-    QSplashScreen splashScreen(pixmap);
-    splashScreen.show();
+    //QSplashScreen splashScreen(pixmap);
+    //splashScreen.show();
 
     // Initialization of Lua engine
-    splashScreen.showMessage("Initialization Lua engine");
+    //splashScreen.showMessage("Initialization Lua engine");
     LuaEngine luaEngine;
-
     Q_UNUSED(luaEngine)
 
     // Start application
     MainWindow w;
-
     w.show();
 
     return a.exec();
